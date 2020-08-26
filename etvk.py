6f212a3bc4842d76341e4d66889cb7ad40dcfcaf
lb-dl <lb_dl@i.ua>
2020-08-26 19:47:50 +0300

    second commit

--- etvk.py
+++ etvk.py
@@ -3,4 +3,5 @@ print("Learn pull requests")
 print("Start working from a new branch")
 print("Hello, Vita")
 print("Hope, we can do our first pull request")
-
+print("If not, ")
+print("We'll try again...")
